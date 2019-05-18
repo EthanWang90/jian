14db754af4d9b09f1b518ba6cae77f79771d9074
Ethan Wang <ethanwang9011@gmail.com>
2019-05-19 00:48:28 +1200

    add header

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {HeaderWrapper} from './style';
+import {HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button} from './style';
 
 class Header extends React.Component{
     render(){
         return(
             <HeaderWrapper>
-                <div>This is header</div>       
+                <Logo></Logo>
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载App</NavItem> 
+                    <NavItem className="right">登录</NavItem> 
+                    <NavItem className="right">Aa</NavItem>  
+                    <NavSearch></NavSearch>
+                </ Nav>
+                <Addition>
+                    <Button className="writing">写文章</Button>
+                    <Button className="reg">注册</Button>    
+                </Addition>      
             </HeaderWrapper>
         )
     }
