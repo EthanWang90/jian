21d8b216fa64ec616fbb35e4bfb67dda36c79634
Ethan Wang <ethanwang9011@gmail.com>
2019-05-20 21:57:47 +1200

    add search info

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
-import {HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, SearchWrapper} from './style';
+import {
+    HeaderWrapper, 
+    Logo, 
+    Nav, 
+    NavItem, 
+    NavSearch, 
+    Addition, 
+    Button, 
+    SearchWrapper,
+    SearchInfo,
+    SearchInfoTitle,
+    SearchInfoSwitch,
+    SearchInfoItem,
+} from './style';
 import {CSSTransition} from 'react-transition-group';
 import { connect }from 'react-redux';
 import * as actionCreators from './store/actionCreators';
+
+const searchInfoShow = (isShow) => {
+    if (isShow){
+        return (
+            <SearchInfo>
+                <SearchInfoTitle>
+                    热门搜索
+                    <SearchInfoSwitch>换一批</SearchInfoSwitch>    
+                </SearchInfoTitle>
+                <div>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                </div>
+            </SearchInfo>
+        )
+    } else {
+        
+    }
+}
 
 const Header = (props) => {
     return(
@@ -28,6 +64,21 @@ const Header = (props) => {
                         ></NavSearch>
                     </CSSTransition>
                     <i className={props.isFocus? 'iconfont isFocus': 'iconfont'}>&#xe60e;</i>
+                    {/* <SearchInfo>
+                        <SearchInfoTitle>
+                            热门搜索
+                            <SearchInfoSwitch>换一批</SearchInfoSwitch>    
+                        </SearchInfoTitle>
+                        <div>
+                            <SearchInfoItem>教育</SearchInfoItem>
+                            <SearchInfoItem>教育</SearchInfoItem>
+                            <SearchInfoItem>教育</SearchInfoItem>
+                            <SearchInfoItem>教育</SearchInfoItem>
+                            <SearchInfoItem>教育</SearchInfoItem>
+                            <SearchInfoItem>教育</SearchInfoItem>
+                        </div>
+                    </SearchInfo> */}
+                    {searchInfoShow(props.isFocus)}
                 </SearchWrapper>                  
             </ Nav>
             <Addition>
